b2e51a84a2334f77e317de542c355017a673f246
Dss-webportal <itintern02@tauraniholdings.com>
2022-11-16 12:14:32 +0400

    web portal

---
+++ statement.js
@@ -0,0 +1,190 @@
+var da = `[
+                {
+                    "VoucherNo": "IN-10408409",
+                    "Date": "13-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034666",
+                    "Debit": "11,208.75",
+                    "Credit": "0.00",
+                    "Balance": "1,003,243.51"
+                },
+                {
+                    "VoucherNo": "IN-10408412",
+                    "Date": "13-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034669",
+                    "Debit": "2,520.00",
+                    "Credit": "0.00",
+                    "Balance": "1,005,763.51"
+                },
+                {
+                    "VoucherNo": "IN-10408594",
+                    "Date": "18-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034699",
+                    "Debit": "7,245.00",
+                    "Credit": "0.00",
+                    "Balance": "1,013,008.51"
+                },
+                {
+                    "VoucherNo": "IN-10408653",
+                    "Date": "19-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034571",
+                    "Debit": "2,394.00",
+                    "Credit": "0.00",
+                    "Balance": "1,015,402.51"
+                },
+                {
+                    "VoucherNo": "IN-10408654",
+                    "Date": "19-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034575",
+                    "Debit": "12,285.00",
+                    "Credit": "0.00",
+                    "Balance": "1,027,687.51"
+                },
+                {
+                    "VoucherNo": "IN-10408890",
+                    "Date": "26-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034828",
+                    "Debit": "2,562.00",
+                    "Credit": "0.00",
+                    "Balance": "1,030,249.51"
+                },
+                {
+                    "VoucherNo": "IN-10408891",
+                    "Date": "26-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034829",
+                    "Debit": "36,303.75",
+                    "Credit": "0.00",
+                    "Balance": "1,066,553.26"
+                },
+                {
+                    "VoucherNo": "IN-10408898",
+                    "Date": "26-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034833",
+                    "Debit": "12,258.75",
+                    "Credit": "0.00",
+                    "Balance": "1,078,812.01"
+                },
+                {
+                    "VoucherNo": "IN-10409022",
+                    "Date": "28-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034781",
+                    "Debit": "16,695.00",
+                    "Credit": "0.00",
+                    "Balance": "1,095,507.01"
+                },
+                {
+                    "VoucherNo": "IN-10409041",
+                    "Date": "29-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034885",
+                    "Debit": "3,096.45",
+                    "Credit": "0.00",
+                    "Balance": "1,098,603.46"
+                },
+                {
+                    "VoucherNo": "IN-10409042",
+                    "Date": "29-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034750",
+                    "Debit": "3,937.50",
+                    "Credit": "0.00",
+                    "Balance": "1,102,540.96"
+                },
+                {
+                    "VoucherNo": "CN-10408078",
+                    "Date": "31-10-2022",
+                    "DueDate": "31-10-2022",
+                    "Narration": "034575..",
+                    "Debit": "0.00",
+                    "Credit": "8,053.50",
+                    "Balance": "1,094,487.46"
+                },
+                {
+                    "VoucherNo": "IN-10409075",
+                    "Date": "31-10-2022",
+                    "DueDate": "28-02-2023",
+                    "Narration": "034575..",
+                    "Debit": "8,053.50",
+                    "Credit": "0.00",
+                    "Balance": "1,102,540.96"
+                },
+                {
+                    "VoucherNo": "IP-10409205",
+                    "Date": "31-10-2022",
+                    "DueDate": "31-10-2022",
+                    "Debit": "0.00",
+                    "Credit": "249,166.58",
+                    "Balance": "853,374.38"
+                },
+                {
+                    "VoucherNo": "IN-10508045",
+                    "Date": "02-11-2022",
+                    "DueDate": "30-03-2023",
+                    "Narration": "034972",
+                    "Debit": "2,058.00",
+                    "Credit": "0.00",
+                    "Balance": "855,432.38"
+                },
+                {
+                    "VoucherNo": "IN-10508046",
+                    "Date": "02-11-2022",
+                    "DueDate": "30-03-2023",
+                    "Narration": "034970",
+                    "Debit": "17,799.60",
+                    "Credit": "0.00",
+                    "Balance": "873,231.98"
+                },
+                {
+                    "VoucherNo": "IN-10508049",
+                    "Date": "02-11-2022",
+                    "DueDate": "30-03-2023",
+                    "Narration": "034880",
+                    "Debit": "1,207.50",
+                    "Credit": "0.00",
+                    "Balance": "874,439.48"
+                },
+                {
+                    "VoucherNo": "IN-10508109",
+                    "Date": "03-11-2022",
+                    "DueDate": "30-03-2023",
+                    "Narration": "034984",
+                    "Debit": "3,843.00",
+                    "Credit": "0.00",
+                    "Balance": "878,282.48"
+                },
+                {
+                    "VoucherNo": "IN-10508130",
+                    "Date": "03-11-2022",
+                    "DueDate": "30-03-2023",
+                    "Narration": "034884",
+                    "Debit": "3,202.50",
+                    "Credit": "0.00",
+                    "Balance": "881,484.98"
+                },
+                {
+                    "VoucherNo": "IN-10508162",
+                    "Date": "04-11-2022",
+                    "DueDate": "30-03-2023",
+                    "Narration": "034970",
+                    "Debit": "12,230.40",
+                    "Credit": "0.00",
+                    "Balance": "893,715.38"
+                },
+                {
+                    "VoucherNo": "IN-10508164",
+                    "Date": "04-11-2022",
+                    "DueDate": "30-03-2023",
+                    "Narration": "035005",
+                    "Debit": "1,501.50",
+                    "Credit": "0.00",
+                    "Balance": "895,216.88"
+                }
+            ]`;
